bfd8232da6dee7baa6adcf9691aed879b7c1776b
MichalStabrawa <mistaaqua@gmail.com>
2021-12-06 00:15:03 +0100

    commit

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./button.module.scss";
 
 const Button = (props) => {
-  return <button className={styles.buttonDefault}>Click</button>;
+  return <button className={styles.buttonDefault}>Click new</button>;
 };
 
 export default Button;
